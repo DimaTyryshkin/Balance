388a881c4044e91849f468f449f8cb3a2887cc74
ДимаТырышкин <yabloko9392@gmail.com>
2022-06-21 01:27:18 +0700

    Переделал на MVVM
    Переделал BalancePage в стиле MVVM

---
+++ Balance/ViewModels/BalanceViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Balance.Models;
+
+namespace Balance.ViewModels
+{
+    public class BalanceViewModel : ViewModel
+    {
+        public string Balance { get; set; }
+
+        public BalanceViewModel()
+        {
+            using (SQLiteContext db = new SQLiteContext())
+            {
+                Balance = CalculateActualBalance(db.Operations).ToString();
+            }
+        }
+
+        /// <summary>
+        /// Возвращает баланс, рассчитанный на основе истории операций.
+        /// </summary>
+        int CalculateActualBalance(DbSet<Operation> operations)
+        {
+            int summ = 0;
+            foreach (Operation operation in operations.ToList())
+            {
+
+                if (operation.Type == Operation.incomeTypeAlias)
+                {
+                    summ += operation.Summ;
+                    continue;
+                }
+
+                if (operation.Type == Operation.expensesTypeAlias)
+                {
+                    summ -= operation.Summ;
+                    continue;
+                }
+
+                throw new Exception($"Неизвестный тип операции '{operation.Type}'");
+            }
+
+            return summ;
+        }
+    }
+}
--- Balance/Views/BalancePage.xaml.cs
+++ Balance/Views/BalancePage.xaml.cs
@@ -1,8 +1,5 @@
-﻿using Balance.Models;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Linq;
-using Windows.UI.Xaml.Controls;
+﻿using Windows.UI.Xaml.Controls;
+using Balance.ViewModels;
 
 namespace Balance.Views
 {
@@ -12,42 +9,7 @@ namespace Balance.Views
         {
             this.InitializeComponent();
 
-            Draw();
-        }
-
-        void Draw()
-        {
-            using (SQLiteContext db = new SQLiteContext())
-            {
-                balanceText.Text = CalculateActualBalance(db.Operations).ToString();
-            }
-        }
-
-        /// <summary>
-        /// Возвращает баланс, рассчитанный на основе истории операций.
-        /// </summary>
-        int CalculateActualBalance(DbSet<Operation> operations)
-        {
-            int summ = 0;
-            foreach (Operation operation in operations.ToList())
-            {
-
-                if (operation.Type == Operation.incomeTypeAlias)
-                {
-                    summ += operation.Summ;
-                    continue;
-                }
-
-                if (operation.Type == Operation.expensesTypeAlias)
-                {
-                    summ -= operation.Summ;
-                    continue;
-                }
-
-                throw new Exception($"Неизвестный тип операции '{operation.Type}'");
-            }
-
-            return summ;
+            DataContext = new BalanceViewModel();
         }
     }
 }
